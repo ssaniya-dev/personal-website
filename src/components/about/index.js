373bfa902b3d904b04a582ed1331098cd27b585e
Saniya Savla <ssaniyasavla@gmail.com>
2022-10-03 22:55:02 -0400

    hosted resume

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Socials from '../duplicate/socials'
 import './about.css'
+import Pdf from '/Users/saniyasavla/Desktop/Personal Projects/personal-website/src/components/data/saniya_savla_resume.pdf';
+
 function About() {
     return (
         <div className='about'>
@@ -8,7 +10,7 @@ function About() {
                 <p>Hello! I'm Saniya, a student studying computer science at <span className='college'>Georgia Tech</span>. Nice to meet you :)</p>
             </div>
             <div className='resume-btn-div'>
-                <a href="https://drive.google.com/file/d/1OVHv4fvPf9d_UTbhaHtlc2qxkpczPEFI/view?usp=sharing" target="_blank" className='resume-btn'>Here is my Resume!</a>
+                <a href={Pdf} target="_blank" className='resume-btn'>Here is my Resume!</a>
             </div>
             <Socials/>
         </div>
