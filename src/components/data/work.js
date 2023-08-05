75b1bac3b3c0dd62167f5ca93f745896d9e67efd
Saniya Savla <ssavla30@gatech.edu>
2023-08-05 15:25:40 -0400

    added honeywell information

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -2,7 +2,7 @@ export const WorkArray=[
     {
         name: "Honeywell", 
         title: "Incoming Software Engineering Intern", 
-        description: "TBA",
+        description: "Increased retrieval speed of airbase asset information by 45% by using LLM transformer models and PyTorch to develop an NLP chatbot.",
         dateJoin: "May 2023", 
         dateLeave: "August 2023", 
         logo: "https://download.logo.wine/logo/Honeywell/Honeywell-Logo.wine.png"
@@ -10,7 +10,7 @@ export const WorkArray=[
     {
         name: "Vantage Risk", 
         title: "Software Engineering Intern", 
-        description:"Developed a dynamic React dashboard that collects API calls from a python function app to display license usage data across eight developer platforms.",
+        description:"Developed a dynamic React dashboard that collects API calls from a Python function app to display license usage data across eight developer platforms.",
         dateJoin: "June 2022", 
         dateLeave: "August 2022", 
         logo: "https://www.vantagerisk.com/wp-content/uploads/2022/02/Vantage-Blog-Logo.jpg"
@@ -18,7 +18,7 @@ export const WorkArray=[
     {
         name: "Johns Hopkins Applied Physics Laboratory",
         title: "ASPIRE Intern", 
-        description: "Developed a python graphical user interface that compares light curves based on different specifications and assisting in creating python ROS nodes to send communication signals in lunar rover travel.",
+        description: "Developed a python graphical user interface that compares light curves based on different specifications and assisting in creating Python ROS nodes to send communication signals in lunar rover travel.",
         dateJoin: "October 2021", 
         dateLeave: "May 2022",
         logo:"https://pbs.twimg.com/profile_images/907353974226935808/xD9Bfb8v_400x400.jpg",
